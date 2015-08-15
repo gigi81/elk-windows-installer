1fb6a0e4689a3de7dad3ef1eff58b139c7f59061
Luigi Grilli <luigi.grilli@gmail.com>
2015-08-15 15:27:07 +0100

    Changed version to 0.9.0

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ SET NSIS=tools\nsis\makensis.exe
 SET NSSM=tools\nssm\win64\nssm.exe
 SET ZIP=tools\7zip\7za.exe
 
-SET VERSION=1.0.0
+SET VERSION=0.9.0
 SET ELASTIC_SEARCH_VERSION=1.7.1
 SET LOGSTASH_VERSION=1.5.3
 SET KIBANA_VERSION=4.1.1
@@ -11,17 +11,17 @@ SET KIBANA_VERSION=4.1.1
 rem ---------- Download packages ----------
 if not exist "downloads" mkdir downloads
 
-%CURL% "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-%ELASTIC_SEARCH_VERSION%.zip" -o downloads\elasticsearch.zip
-%CURL% "https://download.elastic.co/logstash/logstash/logstash-%LOGSTASH_VERSION%.zip" -o downloads\logstash.zip
-%CURL% "https://download.elastic.co/kibana/kibana/kibana-%KIBANA_VERSION%-windows.zip" -o downloads\kibana.zip
+rem %CURL% "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-%ELASTIC_SEARCH_VERSION%.zip" -o downloads\elasticsearch.zip
+rem %CURL% "https://download.elastic.co/logstash/logstash/logstash-%LOGSTASH_VERSION%.zip" -o downloads\logstash.zip
+rem %CURL% "https://download.elastic.co/kibana/kibana/kibana-%KIBANA_VERSION%-windows.zip" -o downloads\kibana.zip
 rem --------------------------------------
 
 rem ---------- Unzip packages ----------
 if not exist "temp" mkdir temp
 
-%ZIP% x -otemp downloads\elasticsearch.zip
-%ZIP% x -otemp downloads\logstash.zip
-%ZIP% x -otemp downloads\kibana.zip
+rem %ZIP% x -otemp downloads\elasticsearch.zip
+rem %ZIP% x -otemp downloads\logstash.zip
+rem %ZIP% x -otemp downloads\kibana.zip
 
 move temp\elasticsearch-%ELASTIC_SEARCH_VERSION% temp\elasticsearch
 move temp\logstash-%LOGSTASH_VERSION% temp\logstash
