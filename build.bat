d6d80de74f081fb044b2e161b8199f2d3472a6ad
Luigi Grilli <luigi.grilli@gmail.com>
2016-03-05 10:26:05 +0000

    Upgraded to latest elastic v2 and latest components versions

--- build.bat
+++ build.bat
@@ -3,15 +3,15 @@ SET NSIS=tools\nsis\makensis.exe
 SET NSSM=tools\nssm\win64\nssm.exe
 SET ZIP=tools\7zip\7za.exe
 
-SET VERSION=0.9.2
-SET ELASTIC_SEARCH_VERSION=1.7.1
-SET LOGSTASH_VERSION=1.5.3
-SET KIBANA_VERSION=4.1.1
+SET VERSION=1.0.0
+SET ELASTIC_SEARCH_VERSION=2.2.0
+SET LOGSTASH_VERSION=2.2.2
+SET KIBANA_VERSION=4.4.1
 
 rem ---------- Download packages ----------
 if not exist "downloads" mkdir downloads
 
-if not exist "downloads\elasticsearch-%ELASTIC_SEARCH_VERSION%.zip" %CURL% "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-%ELASTIC_SEARCH_VERSION%.zip" -o downloads\elasticsearch-%ELASTIC_SEARCH_VERSION%.zip
+if not exist "downloads\elasticsearch-%ELASTIC_SEARCH_VERSION%.zip" %CURL% "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/%ELASTIC_SEARCH_VERSION%/elasticsearch-%ELASTIC_SEARCH_VERSION%.zip" -o downloads\elasticsearch-%ELASTIC_SEARCH_VERSION%.zip
 if not exist "downloads\logstash-%LOGSTASH_VERSION%.zip" %CURL% "https://download.elastic.co/logstash/logstash/logstash-%LOGSTASH_VERSION%.zip" -o downloads\logstash-%LOGSTASH_VERSION%.zip
 if not exist "downloads\kibana-%KIBANA_VERSION%.zip" %CURL% "https://download.elastic.co/kibana/kibana/kibana-%KIBANA_VERSION%-windows.zip" -o downloads\kibana-%KIBANA_VERSION%.zip
 rem --------------------------------------
